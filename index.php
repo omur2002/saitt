293d75b4f409b23e37612472e05618f2275444a4
omur2002 <75971589+omur2002@users.noreply.github.com>
2021-05-21 11:30:35 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PRACTICE</title>
+	<link rel="stylesheet"href="main.css">
+</head>
+<body>
+	
+	<h1><a href="https://www.instagram.hcom/omk_akg/">omk_akg</a></h1>
+	<img src="https://i.ytimg.com/vi/Pt-JTzpFU-w/maxresdefault.jpg" alt="avatar" width="300" height="300" style="border-radius: 50%">
+
+	<h4 style="background-color:DodgerBlue;">Avalbekov Omurbek</h4> 
+
+<h2><table style="width:100%"></h2>
+  <tr>
+    <th><a href="file:///C:/Users/admin/Desktop/my%20work/followers.html">Followers</a></th>
+    <th><a href="file:///C:/Users/admin/Desktop/my%20work/following.html">Following</a></th>
+
+
+
+</body>
+</html>
